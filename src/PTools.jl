c796bd858480a9e42742a35a37c505bc82f8fef1
tan <tanmaykm@gmail.com>
2013-06-04 12:17:26 +0530

    added queued tasks

--- src/PTools.jl
+++ src/PTools.jl
@@ -2,6 +2,13 @@ module PTools
 
 include("srvr_task.jl")
 include("j_shm.jl")
+include("queued_tasks.jl")
+
+global _debug = false
+function _set_debug(d)
+    global _debug
+    _debug = d
+end
 
 #export STask
 #export Jshm
